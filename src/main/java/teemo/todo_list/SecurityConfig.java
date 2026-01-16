a54bd9bbc756eaf2af2f8e80a929ac2918d28fa6
Nguyễn Xuân Tuyên <xuantuyen81203@gmail.com>
2026-01-16 14:18:31 +0900

    login機能を追加

--- src/main/java/teemo/todo_list/SecurityConfig.java
+++ src/main/java/teemo/todo_list/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -13,16 +15,23 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                return http
-                                .authorizeHttpRequests(
-                                                auth -> auth.requestMatchers("/login", "/register", "/css/**", "/js/**")
-                                                                .permitAll().anyRequest().authenticated())
-                                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home")
-                                                .failureUrl("/login?error=true")
-                                                .permitAll())
-                                .logout(Customizer.withDefaults())
-                                .build();
-        }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers("/login", "/register", "/css/**", "/js/**")
+								.permitAll().anyRequest().authenticated())
+				.formLogin(form -> form.loginPage("/login")
+						.loginProcessingUrl("/login")
+						.defaultSuccessUrl("/")
+						.failureUrl("/login?error=true")
+						.permitAll())
+				.logout(Customizer.withDefaults())
+				.build();
+	}
+	
+	 @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder(); 
+	    }
 }
--- src/main/java/teemo/todo_list/service/impl/AccountServiceImpl.java
+++ src/main/java/teemo/todo_list/service/impl/AccountServiceImpl.java
@@ -1,12 +1,13 @@
 package teemo.todo_list.service.impl;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import teemo.todo_list.model.Account;
-import teemo.todo_list.repository.UserRepository;
+import teemo.todo_list.repository.AccountRepository;
 import teemo.todo_list.service.AccountService;
 
 @Service
@@ -14,16 +15,17 @@ import teemo.todo_list.service.AccountService;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AccountServiceImpl implements AccountService {
 
-    final UserRepository userRepository;
+    final AccountRepository accountRepository;
+    final PasswordEncoder passwordEncoder;
 
     @Override
     public void register(String username, String email, String password) throws IllegalStateException {
-        userRepository.findByEmail(email)
+    	accountRepository.findByEmail(email)
                 .ifPresent(user -> {
                     throw new IllegalStateException("Email already in use");
                 });
 
-        userRepository.save(new Account(username, email, password));
+    	accountRepository.save(new Account(username, email, passwordEncoder.encode(password)));
     }
 
 }
